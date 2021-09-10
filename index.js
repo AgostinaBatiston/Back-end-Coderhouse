bfbbfdb56da273e5918055d0370d0fd6b2999918
Agostina Batiston <agbatiston@hotmail.com>
2021-09-09 23:50:57 -0300

    Estoy tratando de hacer los add y el count

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 class Usuario {
-    constructor(nombre, apellido, libros, mascotas) {
-        this.nombre = nombre,
-            this.apellido = apellido,
-            this.libros = libros,
-            this.mascotas = mascotas;
+    constructor(datos) {
+        this.nombre = datos.nombre,
+            this.apellido = datos.apellido,
+            this.libros = datos.libros,
+            this.mascotas = datos.mascotas;
     }
 
     getFullName() {
@@ -12,7 +12,14 @@ class Usuario {
 
 }
 
-let usuario1 = new Usuario("Agostina", "Batiston", "[{'Dracula', 'Bram Stoker'}, {'La biblioteca secreta', 'Haruki Murakami'}, { 'El eternauta' , 'Hector German Oesterheld'}]", "[gato, gata, conejo]")
+let obj = {
+    nombre: "Agostina",
+    apellido: "Batiston",
+    libros: "[{'Dracula', 'Bram Stoker'}, {'La biblioteca secreta', 'Haruki Murakami'}, { 'El eternauta' , 'Hector German Oesterheld'}]",
+    mascotas: "[gato, gata, conejo]"
+}
+
+let usuario1 = new Usuario(obj)
 
 console.log(usuario1)
 console.log(usuario1.getFullName())
