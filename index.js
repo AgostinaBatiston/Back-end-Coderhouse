6557c4e8202250bc5c3664a7ca11323bed67298c
Agostina Batiston <agbatiston@hotmail.com>
2021-09-09 22:45:16 -0300

    Segui probando mas metodos para el desafio

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 class Usuario {
     constructor(nombre, apellido, libros, mascotas) {
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.libros = libros;
-        this.mascotas = mascotas;
+        this.nombre = nombre,
+            this.apellido = apellido,
+            this.libros = libros,
+            this.mascotas = mascotas;
     }
 
-    
+    getFullName() {
+        return "El usuario es: " + this.nombre + " " + this.apellido
+    }
 
 }
 
+let usuario1 = new Usuario("Agostina", "Batiston", "[{'Dracula', 'Bram Stoker'}, {'La biblioteca secreta', 'Haruki Murakami'}, { 'El eternauta' , 'Hector German Oesterheld'}]", "[gato, gata, conejo]")
+
+console.log(usuario1)
+console.log(usuario1.getFullName())
