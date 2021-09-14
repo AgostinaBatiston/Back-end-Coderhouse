ea15049413322c832168d5d68d1cb0cf92c814a6
Agostina Batiston <agbatiston@hotmail.com>
2021-09-14 17:54:28 -0300

    Ya realice el desafio 1 con todos los métodos solicitados

--- index.js
+++ index.js
@@ -1,27 +1,46 @@
+const libros = [{ nombre: 'Dracula', autor: 'Bram Stoker' }, { nombre: 'La biblioteca secreta', autor: 'Haruki Murakami' }, { nombre: 'El eternauta', autor: 'Hector German Oesterheld' }]
+
 class Usuario {
-    constructor(datos) {
-        this.nombre = datos.nombre,
-            this.apellido = datos.apellido,
-            this.libros = datos.libros,
-            this.mascotas = datos.mascotas;
+    constructor(nombre, apellido, libros, mascotas) {
+        this.nombre = nombre,
+            this.apellido = apellido,
+            this.libros = libros,
+            this.mascotas = mascotas;
     }
 
     getFullName() {
         return "El usuario es: " + this.nombre + " " + this.apellido
     }
 
+    getBookNames() {
+        const Arr = libros.map(libros => libros.nombre)
+        return Arr;
+    }
 
+    addNewBook(nombre2, autor2) {
+        const addBook = { nombre: nombre2, autor: autor2 }
+        this.libros.push(addBook)
+
+    }
+
+    addMascota(mascota) {
+        this.mascotas.push(mascota)
+    }
+
+    CountMascotas() {
+        return "Tiene " + this.mascotas.length + " mascotas"
+
+    }
 
 }
 
-let obj = {
-    nombre: "Agostina",
-    apellido: "Batiston",
-    libros: "[{'Dracula', 'Bram Stoker'}, {'La biblioteca secreta', 'Haruki Murakami'}, { 'El eternauta' , 'Hector German Oesterheld'}]",
-    mascotas: "[gato, gata, conejo]"
-}
 
-let usuario1 = new Usuario(obj)
+let usuario1 = new Usuario("Agostina", "Batiston", libros, ["perro", "gato", "conejo"])
 
-console.log(usuario1)
+
 console.log(usuario1.getFullName())
+usuario1.addNewBook("Uzumaki", "Junji ito")
+console.log(usuario1.getBookNames())
+console.log(usuario1.CountMascotas())
+usuario1.addMascota("gata")
+console.log(usuario1.mascotas)
